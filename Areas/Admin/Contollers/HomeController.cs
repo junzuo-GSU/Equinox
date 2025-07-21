6d45fb8f1e6473c6c6e14e30b6d4b3e548f0552a
junzuo-GSU <jzuo@govst.edu>
2025-07-21 18:00:54 -0500

    Update HomeController.cs

--- Areas/Admin/Contollers/HomeController.cs
+++ Areas/Admin/Contollers/HomeController.cs
@@ -4,7 +4,8 @@ namespace Equinox.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class HomeController : Controller
-    {
+    {   
+       //Below attribute routing is wrong. It can also use more simplified version. Know how?
         [Route("Admin")]
         [Route("Admin/Home")]
         [Route("Admin/Home/Index")]
